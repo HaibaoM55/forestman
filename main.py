2ab46e8adb7f86332561924726c4b551f4145c00
HaibaoM55 <96226102+HaibaoM55@users.noreply.github.com>
2022-08-25 14:04:13 +0300

    alpha

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pygame
 import random
+
+pygame.init()
+
 x = 400
 y = 400
 width = 70
 height = 70
 vel = 3
 
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 600), pygame.FULLSCREEN)
 
     
-pygame.display.set_caption("Forestman beta")
+pygame.display.set_caption("Forestman Alpha")
 
 player = pygame.sprite.Sprite()
 player.image = pygame.image.load("player.png")
@@ -72,12 +75,37 @@ inventory.rect.x = 644
 inventory.rect.y = 0
 
 clock = pygame.time.Clock()
-bunny_nr = 5
+bunny_nr = random.randint(1, 10)
 bunny_x = []
 bunny_y = []
+bunny_dir = []
+bunny_phase = 1
 for i in range(bunny_nr):
     bunny_x.append(random.randint(0, 800))
     bunny_y.append(random.randint(0, 600))
+    direc = random.randint(1, 2)
+    if direc == 1:
+        bunny_dir.append("left")
+    else:
+        bunny_dir.append("right")
+bunny_phase = False
+font = pygame.font.SysFont(None,36)
+text = font.render("Bunny phase!", True, (0, 119, 255))
+"""
+bunny_nr = random.randint(1, 10)
+bunny_x = []
+bunny_y = []
+bunny_dir = []
+bunny_phase += 1
+for i in range(bunny_nr):
+    bunny_x.append(random.randint(0, 800))
+    bunny_y.append(random.randint(0, 600))
+    direc = random.randint(1, 2)
+    if direc == 1:
+        bunny_dir.append("left")
+    else:
+        bunny_dir.append("right") 
+"""
 def jump(i, force, direction):
     if direction == "left":
         sar = random.randint(force - (force *2), 0)
@@ -91,6 +119,14 @@ bush.image = pygame.image.load("bush.png")
 bush.rect = bush.image.get_rect()
 bush_show = False
 
+bunny_killed = int(0)
+bunny_killed_text = font.render(str(bunny_killed), True, (125, 125, 125))
+game = True
+
+start_btn = pygame.sprite.Sprite()
+start_btn.image = pygame.image.load("start_btn.png")
+
+print("Forestman: Loaded")
 while(True):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -125,6 +161,10 @@ while(True):
                             bunny_nr -= 1
                             bunny_x.remove(bunny_x[i])
                             bunny_y.remove(bunny_y[i])
+                            bunny_dir.remove(bunny_dir[i])
+                            bunny_killed += 1
+                            bunny_killed_text = font.render(str(bunny_killed), True, (125, 125, 125))
+                            print(bunny_killed)
                             break
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
@@ -132,71 +172,84 @@ while(True):
                     bush_show = False
                 else:
                     bush_show = True
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        x -= vel
-    if keys[pygame.K_d]:
-        x += vel
-    if keys[pygame.K_w]:
-        y -= vel
-    if keys[pygame.K_s]:
-        y += vel
-    if keys[pygame.K_LSHIFT]:
-        vel = 6
-    else:
-        vel = 3
-    if keys[pygame.K_1]:
-        gun.image = pygame.image.load("gun.png")
-        gun_type = 1
-    if keys[pygame.K_2]:
-        gun.image = pygame.image.load("axe.png")
-        gun_type = 2
-    clock.tick(60)
-    screen.fill((255, 255, 255))
-    mouse_pos = pygame.mouse.get_pos()
-    gun.rect.y = y
-    if(mouse_pos[0] < gun.rect.x):
-        if(gun_type == 1):
-            gun.rect.x = x - width - 27
-        if(gun_type == 2):
-            gun.rect.x = x - width + 27
-        if l == 0:
-            gun.image = pygame.transform.flip(gun.image, True, False)
-            l = 1
-    if(mouse_pos[0] == gun.rect.x):
-        pass
-    if(mouse_pos[0] > gun.rect.x):
-        gun.rect.x = x + width
-        if l == 1:
-            gun.image = pygame.transform.flip(gun.image, True, False)
-            l = 0
-    for i in range(trees):
-        screen.blit(tree.image, (tree_x[i], tree_y[i]))
-    screen.blit(inventory.image, (inventory.rect.x, inventory.rect.y))
-    for i in range(bunny_nr):
-        if bunny_x[i] >= 800:
-            bunny_x[i] = 0
-        if bunny_x[i] <= 0:
-            bunny_x[i] = 800
-        if bunny_y[i] >= 600:
-            bunny_y[i] = 0
-        if bunny_y[i] <= 0:
-            bunny_y[i] = 600
-        speed = 1
-        if(bunny_x[i] - x < 0 and bunny_x[i] - x > -100 and bunny_y[i] - y < 0 and bunny_y[i] - y > -100):
-            speed = 10
-        if(bunny_x[i] - x > 0 and bunny_x[i] - x < 100 and bunny_y[i] - y > 0 and bunny_y[i] - y < 100):
-            speed = 10
-        if(bush_show):
+                    vel = 1
+    if game:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            x -= vel
+        if keys[pygame.K_d]:
+            x += vel
+        if keys[pygame.K_w]:
+            y -= vel
+        if keys[pygame.K_s]:
+            y += vel
+        if keys[pygame.K_LSHIFT]:
+            if bush_show:
+                vel = 2
+            else:
+                vel = 6
+        else:
+            if bush_show:
+                vel = 1
+            else:
+                vel = 3
+        if keys[pygame.K_1]:
+            gun.image = pygame.image.load("gun.png")
+            gun_type = 1
+        if keys[pygame.K_2]:
+            gun.image = pygame.image.load("axe.png")
+            gun_type = 2
+        clock.tick(60)
+        screen.fill((255, 255, 255))
+        mouse_pos = pygame.mouse.get_pos()
+        gun.rect.y = y
+        if(mouse_pos[0] < gun.rect.x):
+            if(gun_type == 1):
+                gun.rect.x = x - width - 27
+            if(gun_type == 2):
+                gun.rect.x = x - width + 27
+            if l == 0:
+                gun.image = pygame.transform.flip(gun.image, True, False)
+                l = 1
+        if(mouse_pos[0] == gun.rect.x):
+            pass
+        if(mouse_pos[0] > gun.rect.x):
+            gun.rect.x = x + width
+            if l == 1:
+                gun.image = pygame.transform.flip(gun.image, True, False)
+                l = 0
+        for i in range(trees):
+            screen.blit(tree.image, (tree_x[i], tree_y[i]))
+        screen.blit(inventory.image, (inventory.rect.x, inventory.rect.y))
+        for i in range(bunny_nr):
+            if bunny_x[i] >= 800:
+                bunny_x[i] = 0
+            if bunny_x[i] <= 0:
+                bunny_x[i] = 800
+            if bunny_y[i] >= 600:
+                bunny_y[i] = 0
+            if bunny_y[i] <= 0:
+                bunny_y[i] = 600
             speed = 1
-        screen.blit(bunny.image, (bunny_x[i], bunny_y[i]))
-        jump(i, speed, "left")
-    screen.blit(enemy.image, enemy.rect)
-    screen.blit(gun.image, (gun.rect.x, gun.rect.y))
-    screen.blit(enemy.image, enemy.rect)
-    screen.blit(player.image, (x, y))
-    if bush_show:
-        bush.rect.x = x 
-        bush.rect.y = y + 20
-        screen.blit(bush.image, bush.rect)
+            if(bunny_x[i] - x < 0 and bunny_x[i] - x > -100 and bunny_y[i] - y < 0 and bunny_y[i] - y > -100):
+                speed = 10
+            if(bunny_x[i] - x > 0 and bunny_x[i] - x < 100 and bunny_y[i] - y > 0 and bunny_y[i] - y < 100):
+                speed = 10
+            if(bush_show):
+                speed = 1
+            screen.blit(bunny.image, (bunny_x[i], bunny_y[i]))
+            jump(i, speed, bunny_dir[i])
+        screen.blit(enemy.image, enemy.rect)
+        screen.blit(gun.image, (gun.rect.x, gun.rect.y))
+        screen.blit(enemy.image, enemy.rect)
+        screen.blit(player.image, (x, y))
+        if bush_show:
+            bush.rect.x = x 
+            bush.rect.y = y + 20
+            screen.blit(bush.image, bush.rect)
+        if bunny_phase:
+            screen.blit(text, (300, 100))
+        screen.blit(bunny_killed_text, (775, 25))
+    if game == False:
+        screen.blit(start_btn, (100, 100))
     pygame.display.update()
